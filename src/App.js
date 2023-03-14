d78b0920757c03bcd16ff7ff80ca382e7c5772bd
Zenny-Aledeh <113387824+Zenny-Aledeh@users.noreply.github.com>
2023-03-14 13:05:51 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is coded by Zeinab Abu and is{" "}
-        <a
-          href="https://github.com/Zenny-Aledeh/my-react-weather-app"
-          target="blank"
-        >
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Zeinab Abu and is{" "}
+          <a
+            href="https://github.com/Zenny-Aledeh/my-react-weather-app"
+            target="blank"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
