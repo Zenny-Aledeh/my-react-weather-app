3bf20d6d3c149f83667146470385fae12ed7f9e9
Zenny-Aledeh <113387824+Zenny-Aledeh@users.noreply.github.com>
2023-03-14 12:37:28 +0000

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by Zeinab Abu and is{" "}
+        <a
+          href="https://github.com/Zenny-Aledeh/my-react-weather-app"
+          target="blank"
+        >
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
